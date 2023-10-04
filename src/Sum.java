3eac6e0ab1c7e89a2dae71311bd6d300c0fd8ccc
Rodrigo <rss.999999999@gmail.com>
2023-10-04 19:31:01 -0300

    Array plus array

--- src/Sum.java
+++ src/Sum.java
@@ -1,8 +1,21 @@
 public class Sum {
 
     public static int arrayPlusArray(int[] arr1, int[] arr2) {
-        // arr1 + arr2 is not working...
-        return 0;
+
+        int sumArr1 = 0;
+        int sumArr2 = 0;
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            sumArr1 += arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            sumArr2 += arr2[i];
+        }
+
+
+        return sumArr1 + sumArr2;
     }
 
 }
--- tests/SolutionTest.java
+++ tests/SolutionTest.java
@@ -1,15 +1,39 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
-
-// TODO: Replace examples and use TDD development by writing your own tests
+import java.util.stream.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SolutionTest {
+
     @Test
-    public void sampleTests() {
+    public void basicTests() {
         assertEquals(21, Sum.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
         assertEquals(-21, Sum.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
         assertEquals(15, Sum.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
         assertEquals(2100, Sum.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+    }
+
+
+    private int check(int[] arr1, int[] arr2) {
+        return IntStream.of(arr1).sum() + IntStream.of(arr2).sum();
+    }
+
+    private int randomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    @Test
+    public void randomTests() {
+        for(int i = 0; i< 20; i++) {
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+            for(int j = 0; j < 5; j++) {
+                arr1[j] = this.randomInt(-10000, 10000);
+                arr2[j] = this.randomInt(-10000, 10000);
+            }
+
+            assertEquals(this.check(arr1, arr2), Sum.arrayPlusArray(arr1, arr2));
+        }
     }
 }
